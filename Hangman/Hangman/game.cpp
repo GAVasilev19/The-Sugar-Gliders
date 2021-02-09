95bb251b9c7864ec458268765cbd51cd0170ded9
Tereza Opanska <TNOpanska19@codingburgas.bg>
2021-02-09 16:08:53 +0200

    Make basic code structure. Fix #7

---
+++ Hangman/Hangman/game.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include "menu.h"
+#include "game.h"
+
+using namespace std;
---
+++ Hangman/Hangman/menu.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <iomanip>
+#include "menu.h"
+#include "game.h"
+
+using namespace std;
